ee594adee05994f78c80cc30eeef7a5b50229d71
Nathan Stauffer <nathanstauffer@gmail.com>
2020-01-14 17:00:21 -0800

    Added to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |num|
+  Blog.create!(
+    title: "Blog #{num + 1}",
+    body: "Hooray!"
+  )
+end
+
+puts "10 blogs created"
+
+5.times do |num|
+  Skill.create!(
+    title: "Skill #{num + 1}",
+    percent_utilized: num * 5
+  )
+end
+
+puts "5 skills created"
+
+9.times do |num|
+  Portfolio.create!(
+    title: "Item #{num + 1}",
+    subtitle: "This is item #{num + 1}",
+    body: "#{num + 2} Latin things",
+    main_image: "https://place-hold.it/600x400",
+    thumb_image: "https://place-hold.it/350x200",
+  )
+end
+
+puts "9 portfolios created"
