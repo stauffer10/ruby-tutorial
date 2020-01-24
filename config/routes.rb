94cb5fa941dce18f2ef584548d0897ad99f71372
Nathan Stauffer <nathanstauffer@gmail.com>
2020-01-24 11:00:08 -0500

    Override portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  
-  get 'about', to 'pages#about'
-  get 'contact', to 'pages#contact'
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' 
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
  
   resources :blogs
   
